1a04ea0201d274e289813a0737fa7063395b1f20
Roberto Ribeiro Correa de Oliveira Andrade Filho <filhorr@tcu.gov.br>
2025-05-15 19:56:08 -0300

    novos arquivos

---
+++ src/main/java/FirebaseManager.java
@@ -0,0 +1,40 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirebaseManager {
+    private static FirebaseDatabase database;
+
+    public static void inicializarFirebase() {
+        try {
+
+            File arquivoCredenciais = new File("serviceAccountKey.json");
+            FileInputStream serviceAccount = new FileInputStream(arquivoCredenciais);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://restaurante-monsieur-33b14-default-rtdb.firebaseio.com")
+            .build();
+
+            FirebaseApp.initializeApp(options);
+                
+            database = FirebaseDatabase.getInstance();
+            System.out.println("Conexão com Firebase estabelecida com sucesso!");
+        } catch (IOException e) {
+            System.err.println("Erro ao inicializar Firebase: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static FirebaseDatabase getDatabase() {
+        if (database == null) {
+            inicializarFirebase();
+        }
+        return database;
+    }
+}
+
