b709aebf640e181ce7df4396d6fe8f3f7a079455
Davi Brasileiro Gomes <121982786+Redondave@users.noreply.github.com>
2025-06-24 10:46:34 -0300

    Update ContaBancariaJose.java

--- src/main/java/classes/ContaBancariaJose.java
+++ src/main/java/classes/ContaBancariaJose.java
@@ -5,11 +5,11 @@ public class ContaBancariaJose {
     private float entrada;
     private float saida;
 
-    public float getSaldo() {return saldo;}
-    public float getEntrada() {return entrada;}
-    public float getSaida() {return saida;}
+    public static float getSaldo() {return saldo;}
+    public static float getEntrada() {return entrada;}
+    public static float getSaida() {return saida;}
 
-    public void setSaldo(float saldo) {this.saldo = saldo;}
-    public void setEntrada(float entrada) {this.entrada = entrada;}
-    public void setSaida(float saida) {this.saida = saida;}
+    public static void setSaldo(float saldo) {this.saldo = saldo;}
+    public static void setEntrada(float entrada) {this.entrada = entrada;}
+    public static void setSaida(float saida) {this.saida = saida;}
 }
