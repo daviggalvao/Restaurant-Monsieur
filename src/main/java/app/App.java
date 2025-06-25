3266543f7fbddca3b3f1af37489abe172293439b
Galvao <daviguerra27@gmail.com>
2025-06-25 15:07:57 -0300

    adicionando todas as barras de pesquisa

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,14 +7,10 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Restaurant Monsieur-José - Sistema de Gestão"); // Define o título uma vez
-
-        // Cria a primeira tela (TelaInicial) e a exibe na Stage principal
-        // Nenhuma outra tela é instanciada aqui
-       // TelaInicial tela = new TelaInicial(primaryStage);
-        // tela.mostrarTela(); // Chama o método mostrarTela() da base Tela
-        //primaryStage.show();
-        TelaGerenciarReserva tela = new TelaGerenciarReserva(primaryStage);
+        primaryStage.setTitle("Restaurant Monsieur-José - Sistema de Gestão");
+        // TelaInicial tela = new TelaInicial(primaryStage);
+        // tela.mostrarTela();
+        TelaGerente tela = new TelaGerente(primaryStage);
         tela.mostrarTela();
         primaryStage.show();
     }
