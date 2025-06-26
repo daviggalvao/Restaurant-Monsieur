1d3e014db476fcf7d0944b4a99f8392b2942d964
Galvao <daviguerra27@gmail.com>
2025-06-26 11:33:17 -0300

    fix navegabilidade pagamento

--- src/main/java/app/TelaReserva.java
+++ src/main/java/app/TelaReserva.java
@@ -1,5 +1,6 @@
 package app;
 
+import classes.OrigemDaTela;
 import database.JpaUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
@@ -219,7 +220,7 @@ public class TelaReserva extends Tela { // 1. Garante que herda de Tela
                 em.getTransaction().commit();
                 mostrarAlerta(Alert.AlertType.INFORMATION, Tela.emFrances ? "Réservation effectuée" : "Reserva Realizada", Tela.emFrances ? "Réservation effectuée pour le client "+ name +" avec succès !" : "Reserva feita para o cliente " + name + " com sucesso!");
 
-                new TelaPagamento(getStage(), cliente.getEmail()).mostrarTela();
+                new TelaPagamento(getStage(), cliente.getEmail(), OrigemDaTela.TELA_RESERVA).mostrarTela();
 
             }catch (NoResultException e) {
                 mostrarAlerta(Alert.AlertType.WARNING, Tela.emFrances ? "La recherche a renvoyé une erreur" : "Busca retornou erro", Tela.emFrances ? "Aucun compte avec" + email + " n'a été trouvé" : "Nenhuma conta com o" + email + " foi encontrada");
